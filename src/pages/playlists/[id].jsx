3c2d9baed9f9ea962c7e43672484f159589f25d8
elias kihlstenius <elias.kihlstenius@gmail.com>
2023-03-28 14:54:48 +0200

    fixed grid

--- src/pages/playlists/[id].jsx
+++ src/pages/playlists/[id].jsx
@@ -3,6 +3,9 @@ import React from "react";
 import { spotifyApi } from "../_app";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { formatTime } from "@/utils/formatTime";
+import { Clock } from "react-feather";
+import { Play } from "react-feather";
 
 export default function Playlist() {
     const router = useRouter();
@@ -37,39 +40,51 @@ export default function Playlist() {
                 </div>
             </div>
             <div className="p-10">
-                <table className="w-full">
-                    <tbody>
-                        {playlist.tracks.items.map((item, index) => (
-                            <tr
-                                key={item.id}
-                                className="h-11 whitespace-nowrap text-sm text-text-dimmed hover:bg-text-dimmed/10"
-                            >
-                                <td className="pl-4 text-base">{index + 1}</td>
-                                <td>
-                                    <div className="flex gap-4 overflow-hidden">
-                                        <img
-                                            src={item.track.album.images[0].url}
-                                            alt=""
-                                            className="h-12 w-12"
-                                        />
-                                        <div>
-                                            <h4 className="text-text">
-                                                {item.track.name}
-                                            </h4>
-                                            <p className="">
-                                                {item.track.artists[0].name}
-                                            </p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>{item.track.album.name}</td>
-                                <td className="pr-4">
-                                    {item.track.duration_ms}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+                <div className="w-full text-text-dimmed">
+                    <div className="grid grid-cols-[auto_1fr_1fr_auto] items-center gap-4 px-6">
+                        <div className="w-8">#</div>
+                        <div>Name</div>
+                        <div>Album</div>
+                        <div>
+                            <Clock className="h-5 w-5" />
+                        </div>
+                    </div>
+
+                    <hr className="my-3 border-text-dimmed/30"></hr>
+                    {playlist.tracks.items.map((item, index) => (
+                        <div
+                            key={item.id}
+                            className="group grid grid-cols-[auto_1fr_1fr_auto] items-center gap-4 rounded-md py-1.5 px-3 text-sm  hover:bg-text-dimmed/10"
+                        >
+                            <div className=" w-8 text-base">
+                                <p className="truncate group-hover:hidden">
+                                    {index + 1}
+                                </p>
+                                <Play className="hidden h-5 w-5 fill-text-dimmed group-hover:block" />
+                            </div>
+                            <div className="flex items-center  gap-4 overflow-hidden">
+                                <img
+                                    src={item.track.album.images[0].url}
+                                    alt=""
+                                    className="h-12 w-12"
+                                />
+                                <div className="overflow-hidden">
+                                    <h4 className="truncate text-text">
+                                        {item.track.name}
+                                    </h4>
+                                    <p className="">
+                                        {item.track.artists[0].name}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="truncate">
+                                {item.track.album.name}
+                            </div>
+                            <div>{formatTime(item.track.duration_ms)}</div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </Layout>
     );
