b583dd42de2aabaf030fc1e61d23134bd27db5c0
elias kihlstenius <elias.kihlstenius@gmail.com>
2023-03-28 13:02:19 +0200

    added layout component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,10 @@
-import Sidebar from "@/components/Sidebar";
+import Layout from "@/components/Layout";
 import React from "react";
 
 export default function index() {
     return (
-        <div className="flex h-screen flex-col">
-            <div className="flex flex-1 overflow-y-auto ">
-                <Sidebar />
-                <main className="flex-1 ">main</main>
-            </div>
-            <footer className="h-[59px] ">player</footer>
-        </div>
+        <Layout>
+            <div>hej från main</div>
+        </Layout>
     );
 }
