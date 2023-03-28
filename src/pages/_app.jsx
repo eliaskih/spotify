47e262494797dd8e3585158e411025e1c885effd
elias kihlstenius <elias.kihlstenius@gmail.com>
2023-03-28 10:36:43 +0200

    added react query to fetch

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,12 +3,15 @@ import SpotifyWebApi from "spotify-web-api-node";
 import { redirectURL } from "@/config";
 import { getAccessTokenFromUrl } from "@/utils/getAccessTokenFromUrl";
 import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const spotifyApi = new SpotifyWebApi({
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
     clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
     redirectUri: redirectURL,
 });
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
     const router = useRouter();
@@ -26,5 +29,9 @@ export default function App({ Component, pageProps }) {
         }
     }
 
-    return <Component {...pageProps} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    );
 }
