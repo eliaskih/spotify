10a59e879f26fe7f983bf8dbf468e597fbfd05fe
elias kihlstenius <elias.kihlstenius@gmail.com>
2023-03-30 14:54:42 +0200

    fixed shit:)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,13 @@ import React from "react";
 export default function index() {
     return (
         <Layout>
-            <div>hej från main</div>
+            <div className="p-10">
+                <h1 className="text-4xl font-bold">
+                    Welcome to my spotify clone
+                </h1>
+
+                <div className="grid grid-cols-5"></div>
+            </div>
         </Layout>
     );
 }
